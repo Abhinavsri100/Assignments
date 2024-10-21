3dc670c320d9476afc2ac7831970e1d87ba70317
Abhinav <srivastavabhinav556@gmail.com>
2024-10-22 04:36:34 +0530

    check Anagram

---
+++ Javascript/Anagram.js
@@ -0,0 +1,14 @@
+/**
+ * Write a function `isAnagram` which takes 2 parameters and returns true/false if those are anagrams or not.
+ *   What's Anagram?
+ *   - A word, phrase, or name formed by rearranging the letters of another, such as spar, formed from rasp.
+ *
+ * @format
+ */
+function isAnagram(s1, s2) {
+  if (s1.length() != s2.length()) return false;
+  const a = s1.toLowerCase().split("").sort().join("");
+  const b = s2.toLowerCase().split("").sort().join("");
+  if (a == b) return true;
+  return false;
+}
